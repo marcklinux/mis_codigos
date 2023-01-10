c70b403b1e4b521b18bd96d48574937092a4e074
CLAP2201-2110\Marco Dominguez <marco.dominguez@proveedoresgnp.mx>
2023-01-10 00:36:28 -0600

    array promedio de vueltas uso de metodos

--- ArrayFunciones/src/arrayfunciones/Prueba.java
+++ ArrayFunciones/src/arrayfunciones/Prueba.java
@@ -4,6 +4,8 @@
  */
 package arrayfunciones;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Marco Dominguez
@@ -14,7 +16,55 @@ public class Prueba {
      * un corredor registra N vueltas. la maquina informa cuantas vueltas superan el promedio
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        int nroVueltas = leerEnteroEntre("cuantas vueltas ", 4,10);
+        int[] vueltas = new int[nroVueltas];
+        for(int i = 0; i < vueltas.length;i++){
+            String mensaje = "vuelta " + (i+1)+ " de " + vueltas.length + " : ";
+            vueltas[i]=leerEnteroEntre(mensaje,1,100);
+        }
+        mostrarArreglo(vueltas);
+        int cant = obtenerValoresMayores(vueltas);
+        System.out.println("Valores Mayores " + cant);
+    }
+    
+    static int obtenerValoresMayores(int[] arreglo){
+        int cont=0;
+        double promedio = obtenerPromedio(arreglo);
+        System.out.println("Promedio: " + promedio);
+        for(int i=0;i<arreglo.length;i++){
+            if(arreglo[i]> promedio){
+                cont++;
+            }
+        }
+        return cont;
+    }
+    
+    static double obtenerPromedio(int[] arreglo){
+        int acum = 0;
+        double prom;
+        for(int i = 0; i< arreglo.length;i++){
+            acum += arreglo[i];
+            }
+        prom = (double)acum / arreglo.length;
+        return prom;
+    }
+    
+    static void mostrarArreglo(int[] arreglo){
+        for(int i= 0; i < arreglo.length;i++){
+            System.out.print(arreglo[i] + " ");
+        }
+        System.out.println("");
+    }
+    
+    static int leerEnteroEntre(String cartel,int menor,int mayor){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print(cartel);
+        double x = entrada.nextDouble();
+        while((int) x != x || x < menor || x > mayor){
+            System.out.print("ERROR." + cartel);
+            x = entrada.nextDouble();
+        }
+        return (int)x;
     }
     
 }
